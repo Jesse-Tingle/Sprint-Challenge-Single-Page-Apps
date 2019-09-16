93249d1f602413cf2e87c6840df2838b8f3efb1b
Jesse-Tingle <jtingle0@gmail.com>
2019-09-15 21:43:45 -0400

    fixed navigation to update active class correctly

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Tab, Menu, Icon } from "semantic-ui-react";
-import { NavLink } from "react-router-dom";
+import { Route, NavLink } from "react-router-dom";
 import WelcomePage from './WelcomePage';
 import CharacterList from './CharacterList';
 import LocationsList from './LocationsList';
-import EpisodesList from './EpisodeList';
+import EpisodeList from './EpisodeList';
 
 // TODO: Add missing menu/tabs/nav below
 
@@ -14,24 +14,35 @@ import EpisodesList from './EpisodeList';
 // https://react.semantic-ui.com/elements/button/
 // https://react.semantic-ui.com/collections/breadcrumb/
 
-const Nav = props => <NavLink {...props} />
+const Nav = function(props) {
+                console.log("Nav Props", props)
+                return <NavLink exact activeClassName="active" {...props} />
+}
 
 const panes = [
     {
         menuItem: <Menu.Item key="Home" as={Nav} to={`/`}>Home Page</Menu.Item>,
-        render: () => <Tab.Pane><WelcomePage /></Tab.Pane>
+        render: () => <Tab.Pane>
+                            <Route exact path='/' component={WelcomePage} />
+                        </Tab.Pane>
     },
     {
         menuItem: <Menu.Item key="Characters" as={Nav} to={`/characters`}>Characters</Menu.Item>,
-        render: () => <Tab.Pane><CharacterList /></Tab.Pane>
+        render: () => <Tab.Pane>
+                            <Route path='/characters' component={CharacterList} />
+                        </Tab.Pane>
     },
     {
         menuItem: <Menu.Item key="Locations" as={Nav} to={`/locations`}>Locations</Menu.Item>,
-        render: () => <Tab.Pane><LocationsList /></Tab.Pane>
+        render: () => <Tab.Pane>
+                            <Route path='/locations' component={LocationsList} />
+                        </Tab.Pane>
     },
     {
         menuItem: <Menu.Item key="Episodes" as={Nav} to={`/episodes`}>Episodes</Menu.Item>,
-        render: () => <Tab.Pane><EpisodesList /></Tab.Pane>
+        render: () => <Tab.Pane>
+                            <Route path='/episodes' component={EpisodeList} />
+                        </Tab.Pane>
     }
 ]
 
